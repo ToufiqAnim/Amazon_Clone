14466003601c3dad1d41fa946278f3235be7f656
ToufiqAnim <toufiq9610@gmail.com>
2022-11-17 18:23:34 +0600

    edited

--- client/src/Firebase/firebaseInit.js
+++
@@ -1,7 +0,0 @@
-import { firebaseConfig } from "./firebaseConfig";
-import firebase from "firebase/compat/app";
-const firebaseInitialize = () => {
-  firebase.initializeApp(firebaseConfig);
-};
-
-export default firebaseInitialize;
